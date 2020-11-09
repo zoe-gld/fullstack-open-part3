22dfa0e784a87a0e8926eda37c1c678b019db449
Zoe Goldschmidt <zoe.gld@gmail.com>
2020-11-09 10:13:02 +0000

    Commit before pushing to heroku

--- exercises/index.js
+++ exercises/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-var morgan = require('morgan')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function (req, res) {
   if (req.method === 'POST') {
@@ -96,7 +98,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
