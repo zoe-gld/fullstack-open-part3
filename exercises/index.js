aac3d44f3be19bfd7001d106689d41208ad1eee0
Zoe Goldschmidt <zoe.gld@gmail.com>
2020-11-23 18:04:17 +0000

    Exercices 3-13 and 3-14

--- exercises/index.js
+++ exercises/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(express.static('build'))
@@ -43,14 +47,16 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello from the phonebook</h1>')
 })
 
-app.get('/info', (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people.</p><p>${new Date()}</p>`
-  )
-})
+// app.get('/info', (request, response) => {
+//   response.send(
+//     `<p>Phonebook has info for ${persons.length} people.</p><p>${new Date()}</p>`
+//   )
+// })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -67,39 +73,36 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.map(person => person.name).includes(body.name)) {
-    return response.status(400).json({
-      error: 'Invalid entry - Name must be unique.'
-    })
-  }
+  // if (persons.map(person => person.name).includes(body.name)) {
+  //   return response.status(400).json({
+  //     error: 'Invalid entry - Name must be unique.'
+  //   })
+  // }
 
-  const person = {
-    id: Math.floor(Math.random() * Math.floor(1000000)),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  })
 
-  persons = persons.concat(person)
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const contact = persons.find(person => person.id === id)
-  if (contact) {
-    return response.json(contact)
-  }
-  response.status(404).end()
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(note => note.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   persons = persons.filter(note => note.id !== id)
 
-  response.status(204).end()
-})
+//   response.status(204).end()
+// })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
